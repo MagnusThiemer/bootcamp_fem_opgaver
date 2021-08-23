60615056b48b1fb1e2d446a70bac9a3d9e654076
MagnusThiemer <thiemer.jensen@gmail.com>
2021-08-23 11:06:19 +0200

    Add solution to assignment 'niveau 2 opgave 1' function and structure

---
+++ Opgaver/Niveau2/01/js/index.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+    
+    const buttonsElement = document.querySelector('#buttons');
+    const highlightColor = 'yellow';
+
+    function highlightButton(event){
+        if(event.target.tagName === 'BUTTON'){                              //thekker om der er trykket på en button
+            let highlightElement = document.querySelector('.highlight');    //laver en variabel med den button, der har class highlight
+    
+            if(highlightElement){                                           //slukker for highlight baggrundsfarven 
+                highlightElement.style.backgroundColor = '#eee';
+                highlightElement.classList.remove('highlight');             //fjerner class highlight
+            }
+            event.target.classList.add('highlight');                        //tilføjer class highlight til event target
+            event.target.style.backgroundColor = highlightColor;            //sætter background color på event target til highlight color
+        }
+    }
+
+    buttonsElement.addEventListener('click', event => highlightButton(event));
+
+})
