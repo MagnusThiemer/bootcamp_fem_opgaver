3b2b7ee896379dfe0c5743399e33f3a64ef05bc1
MagnusThiemer <thiemer.jensen@gmail.com>
2021-08-23 09:05:14 +0200

    Add solution 'niveau 1 opgave 2' solution, structure and extra assignment

---
+++ Opgaver/Niveau1/02/js/index.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    let resultat = beregnForhold(4, 3);
+    let resultat2 = beregnForhold2(54, 0);
+
+    function beregnForhold(a, b){
+    return a / b;
+    };
+
+    function beregnForhold2(a, b){
+        if(b === 0){
+            return null;
+        } else {
+            return a / b;
+        }
+    };
+
+    console.log(resultat);
+    console.log(resultat2);
+
+}); //DOM content loaded slut
